c186f38f689f5c6fceafd277223914c3ee1e515d
Folleach <folleach@yandex.ru>
2020-02-20 21:59:23 +0500

    Create project

---
+++ ConsoleUtils/NumberReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Folleach.ConsoleUtils
+{
+    public static class NumberReader<T>
+    {
+        public delegate bool TryParseDelegate(string text, out T value);
+
+        public static T ReadUntilNoResult(string prefix, TryParseDelegate parser)
+        {
+            while (true)
+            {
+                WritePrefix(prefix);
+                if (parser(Console.ReadLine(), out T value))
+                    return value;
+            }
+        }
+
+        public static T ReadOrDefault(string prefix, TryParseDelegate parser)
+        {
+            WritePrefix(prefix);
+            if (parser(Console.ReadLine(), out T value))
+                return value;
+            return default;
+        }
+
+        private static void WritePrefix(string prefix)
+        {
+            if (prefix != null)
+                Console.Write($"{prefix}: ");
+        }
+    }
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,21 @@
+﻿using Folleach.ConsoleUtils;
+using System;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = NumberReader<int>.ReadUntilNoResult("x", int.TryParse);
+            WriteVariable(x);
+            int y = NumberReader<int>.ReadOrDefault("y", int.TryParse);
+            WriteVariable(y);
+        }
+
+        private static void WriteVariable(object value)
+        {
+            Console.WriteLine($"{value.GetType().Name} equals is {value}");
+        }
+    }
+}

287799edacbed249c91be6ac54b4c502385cec44
Folleach <folleach@yandex.ru>
2020-02-26 13:59:37 +0500

    Added 'ExtendedConsole'

---
+++ ConsoleUtils/ConsoleExtended.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Folleach.ConsoleUtils
+{
+    public static class ExtendedConsole
+    {
+        private const string DefaultPattern = "<message>\r\n<stacktrace>";
+
+        public static void WriteException(Exception exception, ConsoleColor color = ConsoleColor.Gray, string pattern = DefaultPattern)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(pattern.Replace("<message>", exception.Message).Replace("<stacktrace>", exception.StackTrace));
+            Console.ForegroundColor = oldColor;
+        }
+    }
+}
